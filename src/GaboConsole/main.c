fe4ad5bcb648c22ccb43f7d8b7008a031e44dbdd
Ferdinand Lukasak <feroalien@hotmail.com>
2017-12-13 08:02:44 +1000

    Attempt to fix project structure 2

--- src/GaboConsole/main.c
+++ src/GaboConsole/main.c
@@ -3,7 +3,30 @@
 #include "GaboIo.h"
 
 
+
+
+void CommandLoopSample(void);
+
+// A=255\nB=2\nA?\n
 int main(void)
 {
+
+	printf("\nDone...");
+	getchar();
+	getchar();
+
 	return 0;
 }
+
+void CommandLoopSample(void)
+{
+	int commandIndex = 0;
+
+	while (1)
+	{
+
+
+		commandIndex++;
+	}
+}
+
