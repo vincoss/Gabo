a80abe97b74ff8a18306655608687bd03796de8b
Ferdinand Lukasak <feroalien@hotmail.com>
2018-01-16 07:56:14 +1000

    Added gabo usart implementation

--- src/GaboAvr/main.c
+++ src/GaboAvr/main.c
@@ -3,19 +3,28 @@
 #include <Usart.h>
 #include <GaboIo.h>
 #include <GaboCommand.h>
+#include <GaboUsart.h>
 
 
 int main(int argc, char *argv[])
 {
 	UsartInitialize();
+	GaboUsartInterruptInitialize();
 
 	uint8_t result = GaboCommandParse("A=255", 0);
 	UsartWriteChar(result);
 
 	while (1)
 	{
-	
-		_delay_ms(100);
+		UsartWriteChar(powerCommand);
+		UsartWriteChar(commandPowertrain);
+
+		GaboCommandRead();
+
+		UsartWriteChar(powerCommand);
+		UsartWriteChar(commandPowertrain);
+
+		_delay_ms(2000);
 	}
 
 	return 0;
---
+++ src/source/GaboUsart.c
@@ -0,0 +1,34 @@
+#include <avr/io.h>	
+#include <avr/interrupt.h>
+#include "Usart.h"
+#include "GaboUsart.h"
+#include "GaboCommand.h"
+
+
+#define FRAMING_ERROR (1<<FE)
+#define PARITY_ERROR (1<<UPE)
+#define DATA_OVERRUN (1<<DOR)
+#define DATA_REGISTER_EMPTY (1<<UDRE)
+#define RX_COMPLETE (1<<RXC)
+
+void GaboUsartInterruptInitialize(void)
+{
+	UCSR0B |= (1 << RXEN0) | (1 << RXCIE0); // Enable the USART Recieve Complete interrupt ( USART_RX )
+	sei(); // Enable the Global Interrupt Enable flag so that interrupts can be processed
+}
+
+ISR(USART_RX_vect)
+{
+	char status, data;
+
+	///status = UCSRA;
+	data = UDR0;
+
+	// TODO:
+	/*if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN)) != 0)
+	{
+		return;
+	};
+*/
+	GaboCommandReadUsart(data);	
+}
